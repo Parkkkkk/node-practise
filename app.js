116022e37e74d1ecff17419db1437813d7d7a03a
Parkkkkk <mb7878@naver.com>
2018-07-23 01:44:01 +0900

    study

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));  
+// public이라는 디렉터리에 정적인 파일을 갔다놓으면 
+//그 정적인 파일을 사용자에게 서비스 할수있다.
 app.get('/', function(req , res) {
 	res.send('Hello hame page');
 });
+
+app.get('/dynamic' , function(req,res){
+	//변수 선언후 ${}안에 변수를 넣어줌으로써 쉽게 코드에 
+	//변수를 넣어 줄 수 있다.
+	var lis = '';
+	for(var i = 0; i<5 ; i++){
+		lis = lis + '<li>coding</li>';
+	}
+	var time = Date();
+	//원래 코드를 넣을수 없지만 ``를 통해서 긴 코드도 삽입이가능하다 
+	var output = `<!DOCTYPE html> 
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Hello Static!~~
+	<ul>
+	${lis}   
+	</ul>
+	${time}
+</body>
+</html>`
+	res.send(output)
+});
+
+app.get('/route', function(req,res) {
+	res.send('Hello Router, <img src = "/img.png">')
+});
+
 
 app.get('/login' , function(req, res) {
 	res.send('Login please');
